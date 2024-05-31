1ee4d6b4dda3a01a79729ed56c594ac0abcd4089
Vijay Kesanakurthi <vijaydurgakamesh@gmail.com>
2024-05-31 04:19:44 +0000

    Refactor. test method naming

--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -3,29 +3,28 @@ package com.thoughtworks.rectangle;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-
 public class RectangleTest {
 
     @Test
-    public void shouldCalculateAreaOfRectangle() {
+    public void shouldCalculateAreaOfRectangleWhenLengthAndBreadthAre4and5 () {
         Rectangle rectangle = new Rectangle(4, 5);
         assertEquals(20, rectangle.calculateArea());
     }
 
     @Test
-    public void shouldCalculateAreaOfSquare() {
+    public void shouldCalculateAreaOfSquareWhenLengthAndBreadthAreEqual() {
         Rectangle rectangle = new Rectangle(5, 5);
         assertEquals(25, rectangle.calculateArea());
     }
 
     @Test
-    public void shouldCalculateAreaOfRectangleWithLengthZero() {
+    public void shouldCalculateAreaOfRectangleWhenLengthZero() {
         Rectangle rectangle = new Rectangle(0, 5);
         assertEquals(0, rectangle.calculateArea());
     }
 
     @Test
-    public void shouldCalculateAreaOfRectangleWithBreadthZero() {
+    public void shouldCalculateAreaOfRectangleWhenBreadthZero() {
         Rectangle rectangle = new Rectangle(5, 0);
         assertEquals(0, rectangle.calculateArea());
     }
